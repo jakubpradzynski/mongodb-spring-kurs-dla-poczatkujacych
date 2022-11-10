44b90067c4026c10c23a4dc9bc677225cc4dc45a
Jakub Prądzyński <jakubpradzynski@gmail.com>
2022-11-10 17:50:54 +0100

    Add setup for MongoDB container for tests.

--- mongodb_basics/src/test/java/pl/jakubpradzynski/mongodb_basics/MongoDBBasicsApplicationTests.java
+++ mongodb_basics/src/test/java/pl/jakubpradzynski/mongodb_basics/MongoDBBasicsApplicationTests.java
@@ -2,12 +2,27 @@ package pl.jakubpradzynski.mongodb_basics;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
 class MongoDBBasicsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
+
+    @Container
+    public static MongoDBContainer container = new MongoDBContainer(DockerImageName.parse("mongo:5.0.13"));
+
+    @DynamicPropertySource
+    static void mongoDbProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", container::getReplicaSetUrl);
+    }
 
 }
