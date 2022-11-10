d87c9410c479c4de1630b54ec72eea058af5b113
Jakub Prądzyński <jakubpradzynski@gmail.com>
2022-11-10 14:42:46 +0100

    Init module for lesson 2

---
+++ mongodb_basics/src/main/java/pl/jakubpradzynski/mongodb_basics/MongoDBBasicsApplication.java
@@ -0,0 +1,13 @@
+package pl.jakubpradzynski.mongodb_basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDBBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDBBasicsApplication.class, args);
+	}
+
+}
